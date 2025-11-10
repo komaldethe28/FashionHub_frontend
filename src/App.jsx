ca1386fb4ff2448263dff922a70ed04d02b03be5
komal <dethekomalv@gmail.com>
2025-11-10 18:51:05 +0530

    Devloped a Functional and Responsive Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Card from './pages/Card'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Order from './pages/Order'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/card' element={<Card/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/placeOrder' element={<PlaceOrder/>}/>
+        <Route path='/product' element={<Product/>}/>
+      </Routes>
+      
+    </div>
   )
 }
 
